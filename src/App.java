70b69a96eb4a544aee6f2b6b841e6a93a1302685
Joel-Betancourt <joelbetancourt91@outlook.com>
2021-07-22 16:15:09 -0500

    Cambie el nombre por  Hola Mundo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hola Mundo");
     }
 }
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -17,3 +17,4 @@ public class Ejercicio2 {
          return cifras;
        }
 }   
+//
